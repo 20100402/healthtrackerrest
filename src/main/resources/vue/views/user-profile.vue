706ef2f8e2c42e13f2003a727439f13eae2926a4
arbazahmed <20100402@mail.wit.ie>
2022-12-30 13:21:50 +0000

    Vue updated

--- src/main/resources/vue/views/user-profile.vue
+++ src/main/resources/vue/views/user-profile.vue
@@ -47,6 +47,51 @@
       <div class="card-footer text-left">
         <p  v-if="activities.length == 0"> No activities yet...</p>
         <p  v-if="activities.length > 0"> Activities so far...</p>
+        <div class="card bg-light mb-3">
+          <div class="card-header">
+            <div class="row">
+              <div class="col-6">
+                Add Activity
+              </div>
+              <div class="col" align="right">
+                <button rel="tooltip" title="Add"
+                        class="btn btn-info btn-simple btn-link"
+                        @click="hideForm =!hideForm">
+                  <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="card-body" :class="{ 'd-none': hideForm}">
+            <form id="addActivity">
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-activity-description">Description</span>
+                </div>
+                <input type="text" class="form-control" v-model="formData.description" name="description" placeholder="Description"/>
+              </div>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-activity-duration">Duration</span>
+                </div>
+                <input type="number" class="form-control" v-model="formData.duration" name="duration" placeholder="Duration"/>
+              </div>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-activity-description">Calories</span>
+                </div>
+                <input type="text" class="form-control" v-model="formData.calories" name="calories" placeholder="Calories"/>
+              </div>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-activity-description">Date</span>
+                </div>
+                <input type="datetime-local" class="form-control" v-model="formData.date" name="date" placeholder="date"/>
+              </div>
+            </form>
+            <button rel="tooltip" title="Update" class="btn btn-info btn-simple btn-link" @click="addActivity()">Add Activity</button>
+          </div>
+        </div>
         <div class="list-group list-group-flush">
           <div class="list-group-item d-flex align-items-start"
                v-for="(activity,index) in activities" v-bind:key="index">
@@ -60,7 +105,70 @@
                 </button>
               </a>
               <button rel="tooltip" title="Delete" class="btn btn-info btn-simple btn-link"
-                      @click="deleteActivity(Activity, index)">
+                      @click="deleteActivity(activity, index)">
+                <i class="fas fa-trash" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-footer text-left">
+        <p  v-if="activities.length == 0"> No FoodTracker Details yet...</p>
+        <p  v-if="activities.length > 0"> FoodTracker Details so far...</p>
+        <div class="card bg-light mb-3">
+          <div class="card-header">
+            <div class="row">
+              <div class="col-6">
+                Add Food Intake details
+              </div>
+              <div class="col" align="right">
+                <button rel="tooltip" title="Add"
+                        class="btn btn-info btn-simple btn-link"
+                        @click="hideForm =!hideForm">
+                  <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="card-body" :class="{ 'd-none': hideForm}">
+            <form id="addFoodTracker">
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-foodtracker-meal">Meal</span>
+                </div>
+                <input type="text" class="form-control" v-model="formData.meal" name="meal" placeholder="Meal"/>
+              </div>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-foodtracker-caloriesintake">Calories Intake</span>
+                </div>
+                <input type="number" class="form-control" v-model="formData.caloriesintake" name="caloriesintake" placeholder="Calories Intake"/>
+              </div>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="input-foodtracker-date">Date</span>
+                </div>
+                <input type="datetime-local" class="form-control" v-model="formData.date" name="date" placeholder="Date"/>
+              </div>
+            </form>
+            <button rel="tooltip" title="Update" class="btn btn-info btn-simple btn-link" @click="addFoodTracker()">Add FoodTracker</button>
+          </div>
+        </div>
+        <div class="list-group list-group-flush">
+          <div class="list-group-item d-flex align-items-start"
+               v-for="(foodtracker,index) in foodTrackerDetails" v-bind:key="index">
+            <div class="mr-auto p-2">
+              <span><a :href="`/foodtrackerdetails/${foodtracker.id}`"> {{ foodtracker.caloriesintake }} for {{ foodtracker.meal }} </a></span>
+            </div>
+            <div class="p2">
+              <a :href="`/foodtrackerdetails/${foodtracker.id}`">
+                <button rel="tooltip" title="Update" class="btn btn-info btn-simple btn-link">
+                  <i class="fa fa-pencil" aria-hidden="true"></i>
+                </button>
+              </a>
+              <button rel="tooltip" title="Delete" class="btn btn-info btn-simple btn-link"
+                      @click="deleteFoodTracker(foodtracker, index)">
                 <i class="fas fa-trash" aria-hidden="true"></i>
               </button>
             </div>
@@ -79,7 +187,9 @@ Vue.component("user-profile", {
     user: null,
     noUserFound: false,
     activities: [],
-    activity: null
+    foodTrackerDetails: [],
+    formData: [],
+    hideForm :true,
   }),
   created: function () {
     const userId = this.$javalin.pathParams["user-id"];
@@ -94,6 +204,11 @@ Vue.component("user-profile", {
         .then(res => this.activities = res.data)
         .catch(error => {
           console.log("No activities added yet (this is ok): " + error)
+        })
+    axios.get(url + `/foodtrackerdetails`)
+        .then(res => this.foodTrackerDetails = res.data)
+        .catch(error => {
+          console.log("No details added yet (this is ok): " + error)
         })
   },
   methods: {
@@ -127,19 +242,68 @@ Vue.component("user-profile", {
             });
       }
     },
-    deleteActivity: function (activity, index) {
+    deleteActivity: function (Activity, index) {
       if (confirm('Are you sure you want to delete this activity? This action cannot be undone.', 'Warning')) {
-        //user confirmed delete
-        const activityId = activity.id;
+        const activityId = Activity.id;
         const url = `/api/activities/${activityId}`;
         axios.delete(url)
             .then(response =>
                 //delete from the local state so Vue will reload list automatically
-                this.users.splice(index, 1).push(response.data))
+                this.activities.splice(index, 1).push(response.data))
             .catch(function (error) {
               console.log(error)
             });
       }
+    },
+    addActivity: function (){
+      const userId = this.$javalin.pathParams["user-id"];
+      const url = `/api/activities`;
+      axios.post(url,
+          {
+            description: this.formData.description,
+            duration: this.formData.duration,
+            calories: this.formData.calories,
+            started: this.formData.date,
+            userId: userId
+          })
+          .then(response => {
+            this.activities.push(response.data)
+            this.hideForm= true;
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    deleteFoodTracker: function (FoodTracker, index) {
+      if (confirm('Are you sure you want to delete this? This action cannot be undone.', 'Warning')) {
+        const foodtrackerId = FoodTracker.id;
+        const url = `/api/foodtrackerdetails/${foodtrackerId}`;
+        axios.delete(url)
+            .then(response =>
+                //delete from the local state so Vue will reload list automatically
+                this.foodTrackerDetails.splice(index, 1).push(response.data))
+            .catch(function (error) {
+              console.log(error)
+            });
+      }
+    },
+    addFoodTracker: function (){
+      const userId = this.$javalin.pathParams["user-id"];
+      const url = `/api/foodtrackerdetails`;
+      axios.post(url,
+          {
+            meal: this.formData.meal,
+            caloriesIntake: this.formData.caloriesintake,
+            date: this.formData.date,
+            userId: userId
+          })
+          .then(response => {
+            this.foodTrackerDetails.push(response.data)
+            this.hideForm= true;
+          })
+          .catch(error => {
+            console.log(error)
+          })
     }
   }
 });
